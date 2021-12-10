50199ef1bd89eaef19e383256ae3f7bf73030a3b
Elyes <elyesvoisin52@gmail.com>
2021-12-10 08:16:20 +0100

    UPDATE file

--- script/script.js
+++ script/script.js
@@ -5,8 +5,8 @@ const root = document.getElementById('root')
 
 
 let paging = (choice) =>{
-    if (choice == "prev"){
-        if (offset != 0){
+    if (choice === "prev"){
+        if (offset !== 0){
             offset -= 50
             url = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`
             root.innerHTML = ''
@@ -26,9 +26,18 @@ document.getElementsByClassName('previous')[0].addEventListener('click',  () =>{
     paging('prev')
 })
 
+document.getElementsByClassName('previous')[1].addEventListener('click',  () =>{
+    paging('prev')
+})
+
 document.getElementsByClassName('next')[0].addEventListener('click',  () =>{
     paging('next')
 })
+
+document.getElementsByClassName('next')[1].addEventListener('click',  () =>{
+    paging('next')
+})
+
 
 let pokeCall = () =>{
     pokeList.forEach((el) =>{
@@ -36,8 +45,10 @@ let pokeCall = () =>{
             .then(resp => resp.json())
             .then(function(data){
                 addPokemon(data)
+
             })
     })
+
 }
 
 let addPokemon = (data) =>{
@@ -63,64 +74,7 @@ let addPokemon = (data) =>{
     types = Array.from(types)
     types.forEach(el =>{
         pokeType = el.innerHTML.replace(/\s/g, '')
-        console.log(pokeType)
-        if (pokeType === 'normal'){
-            el.classList.add('normal')
-        }
-        if (pokeType === 'fighting'){
-            el.classList.add('fighting')
-        }
-        if (pokeType === 'flying'){
-            el.classList.add('flying')
-        }
-        if (pokeType === 'poison'){
-            el.classList.add('poison')
-        }
-        if (pokeType === 'ground'){
-            el.classList.add('ground')
-        }
-        if (pokeType === 'ice'){
-            el.classList.add('ice')
-        }
-        if (pokeType === 'rock'){
-            el.classList.add('rock')
-        }
-        if (pokeType === 'bug'){
-            el.classList.add('bug')
-        }
-        if (pokeType === 'ghost'){
-            el.classList.add('ghost')
-        }
-        if (pokeType === 'steel'){
-            el.classList.add('steel')
-        }
-        if (pokeType === 'fire'){
-            el.classList.add('fire')
-        }
-        if (pokeType === 'water'){
-            el.classList.add('water')
-        }
-        if (pokeType === 'grass'){
-            el.classList.add('grass')
-        }
-        if (pokeType === 'electric'){
-            el.classList.add('electric')
-        }
-        if (pokeType === 'psychic'){
-            el.classList.add('psychic')
-        }
-        if (pokeType === 'dragon'){
-            el.classList.add('dragon')
-        }
-        if (pokeType === 'dark'){
-            el.classList.add('dark')
-        }
-        if (pokeType === 'fairy'){
-            el.classList.add('fairy')
-        }
-        if (pokeType === 'shadow'){
-            el.classList.add('shadow')
-        }
+        el.classList.add(pokeType)
     })
 }
 
