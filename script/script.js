bb9289ffec1c403c63d0fbf9702828438a9f25a1
Elyes <elyesvoisin52@gmail.com>
2021-12-10 11:32:45 +0100

    UPDATE file

--- script/script.js
+++ script/script.js
@@ -1,40 +1,30 @@
 let offset = 0
-let url = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`
+let url = `https://pokeapi.co/api/v2/pokemon?limit=27&offset=${offset}`
 const root = document.getElementById('root')
-
 
 
 let paging = (choice) =>{
     if (choice === "prev"){
         if (offset !== 0){
-            offset -= 50
-            url = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`
+            offset -= 27
+            url = `https://pokeapi.co/api/v2/pokemon?limit=27&offset=${offset}`
             root.innerHTML = ''
             pokeFetch(url)
         }
     }
     else{
-        offset += 50
-        url = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`
+        offset += 27
+        url = `https://pokeapi.co/api/v2/pokemon?limit=27&offset=${offset}`
         root.innerHTML = ''
         pokeFetch(url)
     }
-    console.log(offset)
 }
 
 document.getElementsByClassName('previous')[0].addEventListener('click',  () =>{
     paging('prev')
 })
 
-document.getElementsByClassName('previous')[1].addEventListener('click',  () =>{
-    paging('prev')
-})
-
 document.getElementsByClassName('next')[0].addEventListener('click',  () =>{
-    paging('next')
-})
-
-document.getElementsByClassName('next')[1].addEventListener('click',  () =>{
     paging('next')
 })
 
@@ -45,37 +35,57 @@ let pokeCall = () =>{
             .then(resp => resp.json())
             .then(function(data){
                 addPokemon(data)
-
             })
     })
-
 }
 
-let addPokemon = (data) =>{
+let addPokemon = (data) => {
 
-    pokemonString = `
+    let pokeDiv = document.createElement('div')
+    pokeDiv.classList.add('pokemon')
+
+    pokemonSingle = `
             <img class="pokemon-image" src="${data.sprites.front_default}"/>
-            <h2 class="pokemon-title">${data.id}. ${data.name}</h2>
-            <div class="type">
-                <p class="type-single">${data.types.map((type) => type.type.name).join(' </p> <p class="type-single"> ')}</p>
+            <h2 class="pokemon-title">${data.name}</h2>
+            <div class="pokemon-hp">${data.stats[0].base_stat} / ${data.stats[0].base_stat}</div>
+            <div class="pokemon-barhp"></div>
+            <div class="pokemon-info">
+                <div class="pokemon-info-weight">
+                   <p>Poids</p>
+                   <p>${data.weight}</p>
+                </div>
+                <div class="pokemon-info-type">
+                    <p>Type</p>
+                    <div>
+                        <p class="type-single">${data.types.map((type) => type.type.name).join(' </p> <p class="type-single"> ')}</p>
+                    </div>
+                </div>
+                <div class="pokemon-info-height">
+                   <p>height</p>
+                   <p>${data.height}</p>
+                </div>
             </div>
+            
     `
 
+    pokeDiv.innerHTML = pokemonSingle
 
-
-    let newDiv = document.createElement("div")
-    newDiv.classList.add('pokemon')
-    newDiv.innerHTML = pokemonString
-
-    let currentDiv = document.getElementById('root');
-    currentDiv.appendChild(newDiv)
+    document.getElementById("root").appendChild(pokeDiv)
 
     let types = document.getElementsByClassName('type-single')
     types = Array.from(types)
     types.forEach(el =>{
         pokeType = el.innerHTML.replace(/\s/g, '')
         el.classList.add(pokeType)
+
     })
+
+    pokeDiv.addEventListener('click', () =>{
+        document.body.innerHTML = ""
+        url = `https://pokeapi.co/api/v2/pokemon/${data.name}`
+        singlePoke(url)
+    })
+
 }
 
 let pokeFetch = (url) => {
@@ -90,8 +100,52 @@ let pokeFetch = (url) => {
 pokeFetch(url)
 
 
+let singlePoke = (url) =>{
+    fetch(url)
+        .then(resp => resp.json())
+        .then(function(data){
+            pokemonSingle = `
+                        <div class="pokemon-image">
+                            <img class="pokemon-image-single" src="${data.sprites.front_default}"/>
+                            <img class="pokemon-image-single" src="${data.sprites.front_shiny}"/>
+                        </div>
+                        <h2 class="pokemon-title">${data.name}</h2>
+                        <div class="pokemon-hp">${data.stats[0].base_stat} / ${data.stats[0].base_stat}</div>
+                        <div class="pokemon-barhp"></div>
+                        <div class="pokemon-info">
+                            <div class="pokemon-info-weight">
+                               <p>Poids</p>
+                               <p>${data.weight}</p>
+                            </div>
+                            <div class="pokemon-info-type">
+                                <p>Type</p>
+                                <div>
+                                    <p class="type-single">${data.types.map((type) => type.type.name).join(' </p> <p class="type-single"> ')}</p>
+                                </div>
+                            </div>
+                            <div class="pokemon-info-height">
+                               <p>height</p>
+                               <p>${data.height}</p>
+                            </div>
+                        </div>
+                `
 
+            let newDiv = document.createElement("div")
+            document.body.style.display = "flex"
+            newDiv.classList.add('pokemon', 'position')
+            newDiv.innerHTML = pokemonSingle
 
+            document.body.appendChild(newDiv)
+
+            let types = document.getElementsByClassName('type-single')
+            types = Array.from(types)
+            types.forEach(el =>{
+                pokeType = el.innerHTML.replace(/\s/g, '')
+                el.classList.add(pokeType)
+
+            })
+        })
+}
 
 
 
